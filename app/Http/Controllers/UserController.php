493b8c24094afa3baefc25ca1fc2ef6806f13692
Muhammad Mubashir Khan <50612566+Mubashir23944@users.noreply.github.com>
2022-03-23 22:54:16 +0500

    2nd Commit
    nd Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,5 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function create(){
-        User::create([
-            'name' => 'Biker10',
-            'email' => 'biker10@gmail.com ',
-            'password' => Hash::make('secret'),
-            'role_id' => 2,
 
-        ]);
-        return 'inserted';
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ Route::get('/login', [App\Http\Controllers\Web\UserController::class, 'getLogin'
 
 // Sender Routes
 
-    Route::get('sender/parcels/{id}', [App\Http\Controllers\Web\UserController::class, 'getSenderParcels'])->name('get-sender-parcels');
-    Route::get('sender/parcel/edit/{id}', [App\Http\Controllers\Web\UserController::class, 'getEditSenderParcel'])->name('edit-sender-parcel');
+Route::get('sender/parcels/{id}', [App\Http\Controllers\Web\UserController::class, 'getSenderParcels'])->name('get-sender-parcels');
+Route::get('sender/parcel/edit/{id}', [App\Http\Controllers\Web\UserController::class, 'getEditSenderParcel'])->name('edit-sender-parcel');
 
 
 // Biker Routes
 
-    Route::get('biker/parcels/{id}', [App\Http\Controllers\Web\UserController::class, 'getBikerParcels'])->name('get-biker-parcels');
-    Route::get('biker/parcel/edit/{id}', [App\Http\Controllers\Web\UserController::class, 'getEditBikerParcel'])->name('edit-biker-parcel');
+Route::get('biker/parcels/{id}', [App\Http\Controllers\Web\UserController::class, 'getBikerParcels'])->name('get-biker-parcels');
+Route::get('biker/parcel/edit/{id}', [App\Http\Controllers\Web\UserController::class, 'getEditBikerParcel'])->name('edit-biker-parcel');
 
 
